b1669fdb756e0e8983ca420d360c99031934c374
winter mutt <sejongsan@hotmail.com>
2017-12-30 22:07:57 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,8 +3,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   let index = 0;
   function onKeyDownHandler(e){
-    const key = e.which;
-    if(key === code[index]){
+    // const key = e.which;
+    if(e.which === code[index]){
       index++;
       if(index === code.length){
         alert("Konami!");
