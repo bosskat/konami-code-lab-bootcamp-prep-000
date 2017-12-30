5c5ed0d45d7af8b16a2caf82e11dd780237cec0d
winter mutt <sejongsan@hotmail.com>
2017-12-30 21:49:39 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   let index = 0;
   function onKeyDownHandler(e){
-    const key = parseInt(e.detail || e.which);
+    const key = parseInt(e.which);
     if(key === code[index]){
       index++;
       if(index === code.length){
